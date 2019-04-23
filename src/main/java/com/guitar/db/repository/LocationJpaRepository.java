e40ac7a45bd58956eb743f64bbf2c14e8b08b7f0
Gheorghe-Adrian Curia <gheorgheadrian.curia@ihsmarkit.com>
2019-04-23 12:47:25 +0300

    Keyword: OrderBy

--- src/main/java/com/guitar/db/repository/LocationJpaRepository.java
+++ src/main/java/com/guitar/db/repository/LocationJpaRepository.java
@@ -13,7 +13,7 @@ public interface LocationJpaRepository extends JpaRepository<Location, Long> {
 	
 	List<Location> findByStateIgnoreCaseStartingWith(String stateName);
 	
-	List<Location> findByStateNotLike(String stateName);
+	List<Location> findByStateNotLikeOrderByStateAsc(String stateName);
 	
 	List<Location> findByStateOrCountry(String value, String value2);
 	List<Location> findByStateAndCountry(String value, String value2);
